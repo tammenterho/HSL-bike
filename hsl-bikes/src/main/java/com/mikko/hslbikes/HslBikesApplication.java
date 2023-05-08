274a160b01f732d89c6e2623de2ea102a7443516
tammenterho <terho.mikko@gmail.com>
2023-05-08 13:01:38 +0300

    backend created and 05.csv to mongoDB

---
+++ hsl-bikes/src/main/java/com/mikko/hslbikes/HslBikesApplication.java
@@ -0,0 +1,13 @@
+package com.mikko.hslbikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HslBikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HslBikesApplication.class, args);
+	}
+
+}
