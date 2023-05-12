23dcf8ef8d4689266e69b858598e97ff52c9bff1
tammenterho <terho.mikko@gmail.com>
2023-05-12 16:21:19 +0300

    got data to front, table

--- client/src/components/trips.jsx
+++ client/src/components/trips.jsx
@@ -1,14 +1,31 @@
-
-import {useGetAllTripsQuery} from './../features/apiSlice';
+import { useGetAllTripsQuery } from './../features/apiSlice';
 
 export const Data = () => {
+  const { data } = useGetAllTripsQuery();
+  console.log(data);
 
-const {data} = useGetAllTripsQuery();
-console.log(data)
-
-return (
-<div>
-    data
-</div>
-)
+  return (
+    <div>
+      {data ? (
+        <table>
+          <thead>
+            <tr>
+              <th>Distance</th>
+              <th>Start</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((trip) => (
+              <tr key={trip.id}>
+                <td>{trip.coveredDistance}</td>
+                <td>{trip.departureStationName}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
