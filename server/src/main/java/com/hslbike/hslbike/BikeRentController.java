89e31afa25e987035da7a5f024feee4c01592deb
tammenterho <terho.mikko@gmail.com>
2023-05-29 16:49:39 +0300

    added viewcontroller

--- server/src/main/java/com/hslbike/hslbike/BikeRentController.java
+++ server/src/main/java/com/hslbike/hslbike/BikeRentController.java
@@ -18,27 +18,26 @@ public class BikeRentController {
     @Autowired
     private BikeRentService bservice;
 
-    // @CrossOrigin(origins = "http://localhost:3000")
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
     public ResponseEntity<List<BikeRent>> allTrips(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int limit) {
         List<BikeRent> trips = bservice.getValidTripsPaginated(page * limit, limit);
         return new ResponseEntity<List<BikeRent>>(trips, HttpStatus.OK);
     }
     
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/search")
     public ResponseEntity<List<BikeRent>> searchBikeRents(@RequestParam String searchTerm, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int limit) {
         List<BikeRent> searchResults = bservice.searchBikeRentsPaginated(searchTerm, page * limit, limit);
         return new ResponseEntity<>(searchResults, HttpStatus.OK);
     }
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/countfrom")
     public ResponseEntity<Integer> countTripsByDepartureStation(@RequestParam String stationName) {
         int departureCount = bservice.countTripsWithDepartureStation(stationName);
         return new ResponseEntity<>(departureCount, HttpStatus.OK);
     }
-    @CrossOrigin(origins = "*")
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/countto")
     public ResponseEntity<Integer> countTripsByEndStation(@RequestParam String stationName) {
     	int returnCount = bservice.countTripsWithReturnStation(stationName);
