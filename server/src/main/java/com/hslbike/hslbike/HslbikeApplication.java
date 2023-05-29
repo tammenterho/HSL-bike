69b3aa8223220b2a3882ae5be3cad7f778faf031
tammenterho <terho.mikko@gmail.com>
2023-05-29 16:55:09 +0300

    removed viewcontroller

--- server/src/main/java/com/hslbike/hslbike/HslbikeApplication.java
+++ server/src/main/java/com/hslbike/hslbike/HslbikeApplication.java
@@ -9,42 +9,33 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class HslbikeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(HslbikeApplication.class, args);
-    }
-
-    @RestController
-    public static class ViewController {
-        @GetMapping(value = { "/", "/{[path:^(?!api$).*$}/**" })
-        public ModelAndView forward() {
-            return new ModelAndView("forward:/index.html");
-        }
-    }
-
-    @RestController
-    @RequestMapping("/api")
-    public static class ApiController {
-        @GetMapping("/")
-        public String apiRoot() {
-            return "Hello World";
-        }
-    }
-
-    @Bean
+	public static void main(String[] args) {
+		SpringApplication.run(HslbikeApplication.class, args);
+		
+		
+	}
+	
+	@Bean
     public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
         return container -> {
-            try {
-                container.setAddress(InetAddress.getByName("0.0.0.0"));
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            }
-        };
+			try {
+				container.setAddress(InetAddress.getByName("0.0.0.0"));
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		};
     }
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hello World";
+		
+	}
+	
+	
 }
