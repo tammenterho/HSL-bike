3e95f862746c3531a8c4cff4ed87fc24b699d49d
tammenterho <terho.mikko@gmail.com>
2023-05-29 14:35:23 +0300

    adding port

--- server/src/main/java/com/hslbike/hslbike/HslbikeApplication.java
+++ server/src/main/java/com/hslbike/hslbike/HslbikeApplication.java
@@ -1,7 +1,13 @@
 package com.hslbike.hslbike;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
@@ -12,11 +18,24 @@ public class HslbikeApplication {
 		
 		
 	}
+	
+	@Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> containerCustomizer() {
+        return container -> {
+			try {
+				container.setAddress(InetAddress.getByName("0.0.0.0"));
+			} catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		};
+    }
 
 	@GetMapping("/")
 	public String apiRoot() {
 		return "Hello World";
 		
 	}
+	
 	
 }
