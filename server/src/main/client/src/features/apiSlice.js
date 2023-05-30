e3fedbfb7b924ee607bb7ca2c6d8a7b2ad784cbd
tammenterho <terho.mikko@gmail.com>
2023-05-30 08:53:52 +0300

    removed changes, works locally

--- server/src/main/client/src/features/apiSlice.js
+++ server/src/main/client/src/features/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const tripsApi = createApi({
   reducerPath: 'tripsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/' }),
   endpoints: (builder) => ({
 
     getAllTrips: builder.query({
--- server/src/main/java/com/hslbike/hslbike/StationsController.java
+++ server/src/main/java/com/hslbike/hslbike/StationsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/stations")
+@RequestMapping("api/stations")
 public class StationsController {
     @Autowired
     private StationsService sService;
